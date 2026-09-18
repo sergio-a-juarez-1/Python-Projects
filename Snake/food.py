44e03449181bd91263435ad18fc142c7cc61d569
sergio-a-juarez-1 <sergio.a.juarez.1@gmail.com>
2026-09-18 07:19:17 -0400

    Update food.py

--- Snake/food.py
+++ Snake/food.py
@@ -4,23 +4,18 @@ import random
 turtle_colors = ['lawn green','dark green', 'burlywood', 'white', 'red', "saddle brown", "lime green", "yellow", "cyan",
 "navy", "olive", 'dark khaki', "olive drab", "dark goldenrod"]
 
-
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.turtle_color = ''
         self.shape("turtle")
         self.penup()
-        #self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.turtle_color = random.choice(turtle_colors)
-        self.color(self.turtle_color)
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
-        self.turtle_color = random.choice(turtle_colors)
-        self.color(self.turtle_color)
-        random_x = random.randint(-270,270)
-        random_y = random.randint(-270,270)
+        self.color(random.choice(turtle_colors))
+        # Ensure x and y coordinates are strict multiples of 20 
+        # range(-260, 260) yields multiples: -260, -240 ... 0 ... 240, 260
+        random_x = random.randint(-13, 13) * 20
+        random_y = random.randint(-13, 13) * 20
         self.goto(random_x, random_y)
-
